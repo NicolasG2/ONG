417066329265f8bf4c36612dde732a8181dac862
Nicolas Vieira <72944237+NicolasG2@users.noreply.github.com>
2023-06-24 15:45:37 -0300

    Ablububle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const listaPosts = [
   {
+    id: 1,
     titulo: "Max - Jack Russel Terrier",
     descricao: "Cachorrinho macho que só quer se aparecer então fica fazendo essas poses. É fofo, dócil, amigável e brincalhão, apesar de querer ser o centro das atenções.",
     imgURL: "./img/aparecdio.jpeg",
@@ -7,6 +8,7 @@ const listaPosts = [
   },
 
   {
+    id: 2,
     titulo: "Cleo - Kokoni",
     descricao: "Uma fêmea brincalhona que adora grama, lama, correr e brincar de pegar bolinha. Tem 4 anos, já é castrada e tem todas as vacinas em dia. Nos contate e adote já essa linda mocinha.",
     imgURL: "./img/bagual.jpg",
@@ -14,29 +16,33 @@ const listaPosts = [
   },
 
   {
+    id: 3,
     titulo: "Capitão - Chihuahua",
-    descricao:"Esse chihuahua macho sem um olho é pequeno, energético e cheio de charme. Sua falta de visão não o impede de ser afetuoso e brincalhão, trazendo alegria aos seus donos.",
+    descricao: "Esse chihuahua macho sem um olho é pequeno, energético e cheio de charme. Sua falta de visão não o impede de ser afetuoso e brincalhão, trazendo alegria aos seus donos.",
     imgURL: "./img/cego.jpg",
     botao: "",
   },
 
   {
+    id: 4,
     titulo: "Aleijadinha - Não sei a raça",
-    descricao:"A Aleijadinha, uma vira-lata fêmea sem uma perna é corajosa e cheia de determinação. Sua força de vontade é inspiradora, sendo uma companheira leal e amorosa para seus donos.",
+    descricao: "A Aleijadinha, uma vira-lata fêmea sem uma perna é corajosa e cheia de determinação. Sua força de vontade é inspiradora, sendo uma companheira leal e amorosa para seus donos.",
     imgURL: "./img/perneta.jpeg",
     botao: "",
   },
 
   {
+    id: 5,
     titulo: "Slinky - Salsicha",
-    descricao:"Stinky, o salsicha macho, é um cãozinho engraçado, afetuoso e cheio de energia. Com seu corpo alongado e patas curtas, ele é um companheiro divertido e adorável, capaz de trazer muitas risadas e momentos especiais para sua família.",
+    descricao: "Stinky, o salsicha macho, é um cãozinho engraçado, afetuoso e cheio de energia. Com seu corpo alongado e patas curtas, ele é um companheiro divertido e adorável, capaz de trazer muitas risadas e momentos especiais para sua família.",
     imgURL: "./img/sicha.jpeg",
     botao: "",
   },
 
   {
+    id: 6,
     titulo: "Bob - Vira lata",
-    descricao:"Bob, o vira-lata macho, é um cãozinho amigável, leal e cheio de personalidade. Ele é um companheiro fiel e amoroso, pronto para trazer alegria e diversão para sua família. Com sua natureza descontraída e espírito brincalhão, Bob é um verdadeiro amigo de quatro patas.",
+    descricao: "Bob, o vira-lata macho, é um cãozinho amigável, leal e cheio de personalidade. Ele é um companheiro fiel e amoroso, pronto para trazer alegria e diversão para sua família. Com sua natureza descontraída e espírito brincalhão, Bob é um verdadeiro amigo de quatro patas.",
     imgURL: "./img/viralata.jpeg",
     botao: "",
   },
@@ -45,13 +51,13 @@ const listaPosts = [
 let container = document.querySelector(".container");
 
 const criacaoPosts = () => {
-  listaPosts.forEach((post) => {
+  listaPosts.forEach((animal) => {
     let card = `   
             <div class="card">
-                <img class="card-img" src=${post.imgURL} /> 
-                <h2 class="card-titulo"> ${post.titulo} </h2>
-                <p class="card-descricao"> ${post.descricao} </p>
-                <button class="card-botao type="button""><a href ="wa.me/5541987654321">Entre em contato</a> ${post.botao}</button>
+                <img class="card-img" src=${animal.imgURL} /> 
+                <h2 class="card-titulo"> ${animal.titulo} </h2>
+                <p class="card-descricao"> ${animal.descricao} </p>
+                <a class="card-botao" idAnimal="${animal.id}">ADOTAR </a>
             </div>
         `;
     container.innerHTML += card;
@@ -66,10 +72,16 @@ window.onload = () => {
   cards.forEach((card) => {
     console.log(card);
     card.addEventListener("mousemove", () => {
-  
+
     });
 
-    card.addEventListener("mouseout", ()=> {
+    card.addEventListener("mouseout", () => {
+    });
+
+    card.lastElementChild.addEventListener("click", (event) => {
+      event.preventDefault();
+      sectionStorage.setItem("idAnimal", card.lastElementChild.getAttribute("idAnimal"));
+      window.location.href = "../form.html";
     });
   });
 };
