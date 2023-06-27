efdaf1cdb85c4364a8a53932aae8596a007a70aa
NicolasG2 <72944237+NicolasG2@users.noreply.github.com>
2023-06-27 17:33:42 -0300

    André é um anjo

--- js/alterar-tema.js
+++ js/alterar-tema.js
@@ -1,3 +1,45 @@
+let botaoTema = document.querySelector("#botaoTema");
+console.log(botaoTema);
+
+botaoTema.addEventListener("click", () => {
+  let body = document.querySelector("body");
+  let header = document.querySelector("header");
+  let nav_link = document.querySelector(".nav-link");
+  let nav_link_active = document.querySelector(".nav-link.active");
+  let card = document.querySelector(".card");
+  let card_titulo = document.querySelector(".card-titulo");
+  let card_descricao = document.querySelector(".card-descricao");
+  let card_botao = document.querySelector(".card-botao");
+
+  if (botaoTema.checked) {
+    body.classList.add("body-dark");
+    header.classList.add("header-dark");
+    nav_link.classList.add("nav-link-dark");
+    nav_link_active.classList.add("nav-link-dark.active");
+
+    card.forEach(card => {
+      card.classList.add("card-dark");
+      card_titulo.classList.add("card-dark-titulo");
+      card_descricao.classList.add("card-dark-descricao");
+      card_botao.classList.add("card-dark-botao");
+    });
+
+  } else {
+    body.classList.remove("body-dark");
+    header.classList.remove("header-dark");
+    nav_link_active.classList.remove("nav-link-dark.active");
+
+    card.forEach(card => {
+      card.classList.remove("card-dark");
+      card_titulo.classList.remove("card-dark-titulo");
+      card_descricao.classList.remove("card-dark-descricao");
+      card_botao.classList.remove("card-dark-botao");
+    });
+  }
+});
+
+
+
 /*var linkElement = document.getElementById('dinamico');
 
 let botaoTema = document.querySelector("#botaoTema");
