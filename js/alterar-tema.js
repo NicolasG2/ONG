3b5d68df8b5b0031e700704303b6b3deb24b825c
NicolasG2 <72944237+NicolasG2@users.noreply.github.com>
2023-06-27 12:21:29 -0300

    Terminar

--- js/alterar-tema.js
+++ js/alterar-tema.js
@@ -1,5 +1,22 @@
-/*var FILE = '../css/tema.css';
-document.getElementById('dinamico').setAttribute('href', FILE);
+/*var linkElement = document.getElementById('dinamico');
+
+let botaoTema = document.querySelector("#botaoTema");
+
+botaoTema.addEventListener("click", () => {
+  if (botaoTema.checked) {
+    linkElement.setAttribute('href', '../css/tema.css');
+  } else {
+    linkElement.setAttribute('href', '../css/style.css');
+  }
+});
+
+
+var darkThemeFile = '../css/tema.css';
+var lightThemeFile = '../css/style.css';
+var linkElement = document.getElementById('dinamico');
+
+// Define o arquivo CSS inicial
+linkElement.setAttribute('href', lightThemeFile);
 
 let botaoTema = document.querySelector("#botaoTema");
 console.log(botaoTema);
@@ -8,12 +25,31 @@ botaoTema.addEventListener("click", () => {
   let body = document.querySelector("body");
 
   if (botaoTema.checked) {
-    body.classList.add(FILE);
+    linkElement.setAttribute('href', darkThemeFile); // Atualiza para o arquivo CSS do tema escuro
   } else {
-    body.classList.remove(FILE);
+    linkElement.setAttribute('href', lightThemeFile); // Atualiza para o arquivo CSS do tema claro
   }
 });
-*/
+
+
+var FILE = '../css/tema.css';
+var linkElement = document.getElementById('dinamico');
+linkElement.setAttribute('href', FILE);
+
+let botaoTema = document.querySelector("#botaoTema");
+console.log(botaoTema);
+
+botaoTema.addEventListener("click", () => {
+  let body = document.querySelector("body");
+
+  if (botaoTema.checked) {
+    body.classList.add("tema");
+  } else {
+    body.classList.remove("tema");
+  }
+});
+
+
 var FILE = '../css/tema.css';
 var linkElement = document.getElementById('dinamico');
 linkElement.setAttribute('href', FILE);
@@ -30,3 +66,19 @@ botaoTema.addEventListener("click", () => {
     body.classList.remove("tema"); // Remova a classe específica para o tema
   }
 });
+
+var FILE = '../css/tema.css';
+document.getElementById('dinamico').setAttribute('href', FILE);
+
+let botaoTema = document.querySelector("#botaoTema");
+console.log(botaoTema);
+
+botaoTema.addEventListener("click", () => {
+  let body = document.querySelector("body");
+
+  if (botaoTema.checked) {
+    body.classList.add(FILE);
+  } else {
+    body.classList.remove(FILE);
+  }
+});*/
