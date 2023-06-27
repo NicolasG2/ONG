b62d4347f0cac56a7cc8eb449aea2387e82bbb53
NicolasG2 <72944237+NicolasG2@users.noreply.github.com>
2023-06-27 09:15:20 -0300

    AAAAAAAAAAAAAA

--- js/alterar-tema.js
+++ js/alterar-tema.js
@@ -1,3 +1,6 @@
+/*var FILE = '../css/tema.css';
+document.getElementById('dinamico').setAttribute('href', FILE);
+
 let botaoTema = document.querySelector("#botaoTema");
 console.log(botaoTema);
 
@@ -5,8 +8,25 @@ botaoTema.addEventListener("click", () => {
   let body = document.querySelector("body");
 
   if (botaoTema.checked) {
-    body.classList.add("body-dark");
+    body.classList.add(FILE);
   } else {
-    body.classList.remove("body-dark");
+    body.classList.remove(FILE);
+  }
+});
+*/
+var FILE = '../css/tema.css';
+var linkElement = document.getElementById('dinamico');
+linkElement.setAttribute('href', FILE);
+
+let botaoTema = document.querySelector("#botaoTema");
+console.log(botaoTema);
+
+botaoTema.addEventListener("click", () => {
+  let body = document.querySelector("body");
+
+  if (botaoTema.checked) {
+    body.classList.add("tema"); // Adicione a classe específica para o tema
+  } else {
+    body.classList.remove("tema"); // Remova a classe específica para o tema
   }
 });
